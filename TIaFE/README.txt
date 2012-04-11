d9ba4b1900e2b06d9afcc89a9e738a65ee445a81
Andreas Decker <e0227283@student.tuwien.ac.at>
2012-04-11 18:50:51 +0200

    First

---
+++ TIaFE/README.txt
@@ -0,0 +1,13 @@
+
+Text Indexer and Feature Extractor
+----------------------------------
+
+@author Andreas Decker
+        e0227283@student.tuwien.ac.at
+        2012-04-11
+        
+HowTo:
+
+
+
+
---
+++ TIaFE/src/ir/MyTIandFE.java
@@ -0,0 +1,29 @@
+/**
+ * Text Indexer and Feature Extractor
+ */
+package ir;
+
+
+import weka.core.Instances;
+import weka.core.converters.ArffLoader;
+import weka.core.converters.ArffSaver;
+
+import java.io.File;
+
+
+/**
+ * @author Andreas Decker
+ *         e0227283@student.tuwien.ac.at
+ *
+ */
+public class MyTIandFE {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+	}
+
+}
